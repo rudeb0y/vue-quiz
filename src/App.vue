e3354266eedaab7681bb79bfdb82584ecf3d9a26
Stephen Herron <stephen.herron23@gmail.com>
2019-05-21 00:45:05 +0100

    Adding in questions and answers

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
   <div id="app">
-    <Header />
-    <question-box />
+    <Header
+      :numCorrect="numCorrect"
+      :numTotal="numTotal"
+    />
+    <b-container class="bv-exmaple-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <question-box
+            v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment" />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import QuestionBox from './components/QuestionBox.vue'
+import Header from "./components/Header.vue";
+import QuestionBox from "./components/QuestionBox.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Header,
     QuestionBox,
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    }
+  },
+  methods: {
+    next() {
+      this.index++
+    },
+    increment(isCorrect) {
+      if(isCorrect) {
+        this.numCorrect++
+      }
+      this.numTotal++
+    }
+  },
+  mounted: function() {
+    fetch('https://opentdb.com/api.php?amount=10&type=multiple', {
+      method: 'get'
+    })
+    .then((response) => {
+      return response.json()
+    })
+    .then((jsonData) => {
+      this.questions = jsonData.results
+    })
   }
-}
+
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,128 @@
 <template>
-    <h1>Question Box</h1>
+    <b-jumbotron>
+      <template slot="lead">
+        {{ currentQuestion.question }}
+      </template>
+
+
+      <hr class="my-4" />
+
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer, index) in shuffledAnswers"
+          :key="index"
+          @click.prevent="selectAnswer(index)"
+          :class="answerClass(index)"
+        >
+          {{ answer }}
+        </b-list-group-item>
+      </b-list-group>
+
+
+      <b-button
+        variant="primary"
+        @click="submitAnswer"
+        :disabled="selectedIndex === null || answered"
+      >
+        Submit
+      </b-button>
+      <b-button @click="next">Next</b-button>
+    </b-jumbotron>
 </template>
+<script>
+import _ from 'lodash'
+
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function,
+    increment: Function
+  },
+  data() {
+    return {
+      selectedIndex: null,
+      correctIndex: null,
+      shuffledAnswers: [],
+      answered: false
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index
+    },
+    shuffleAnswers() {
+      let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+      this.shuffledAnswers = _.shuffle(answers)
+      this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+
+    },
+    answerClass(index) {
+      let answerClass = ''
+
+      if (this.answered) {
+        if (index === this.correctIndex) {
+          answerClass = 'correct'
+        }  else if (index === this.selectedIndex) {
+          answerClass = 'incorrect'
+        }
+      } else if (index === this.selectedIndex) {
+        answerClass = 'selected'
+      }
+
+      return answerClass
+    },
+    submitAnswer() {
+      let isCorrect = false
+
+      if(this.selectedIndex === this.correctIndex) {
+        isCorrect = true
+      }
+
+      this.answered = true
+
+      this.increment(isCorrect)
+    }
+  },
+  computed: {
+
+  },
+  watch: {
+    currentQuestion: {
+      immediate: true,
+      handler() {
+        this.selectedIndex = null
+        this.answered = false
+        this.shuffleAnswers()
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+  .list-group {
+    margin-bottom: 15px;
+  }
+
+  .list-group-item:hover {
+    background: #EEE;
+    cursor: pointer;
+  }
+
+  .selected {
+    background-color: lightblue;
+  }
+
+  .correct {
+    background-color: lightgreen;
+  }
+
+  .incorrect {
+    background-color: red;
+  }
+  .btn {
+    margin: 0 5px;
+  }
+
+</style>
+
+
